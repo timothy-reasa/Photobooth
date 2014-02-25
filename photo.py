07b91faa449c71f83bf5935edea1038abf93a970
timothy-reasa <timothy.reasa@gmail.com>
2014-02-24 18:39:28 -0600

    Style updates

--- photo.py
+++ photo.py
@@ -19,6 +19,7 @@ BTN_PHOTO = 22
 OUT_LIGHT = 17
 #OUT_WARNING =
 DELAY_MS = 100
+NUM_IMAGES = 4
 MAX_PRINTS = 25
 DIR_SAVE = "/home/pi/photobooth/captured images/"	#for individual camera snapshots
 DIR_COMPOSITE = "/home/pi/photobooth/final images/" #for final composite images for printing
@@ -65,94 +66,91 @@ off = False
 shutter = False
 
 #Main loop
-root.after(DELAY_MS, checkButton)
+root.after(DELAY_MS, mainBody)
 root.mainloop()
 
 def btn_click():
     gpio.cleanup()
     sys.exit(0)
 
-def checkButton():
-    #checkShutdown()
+def mainBody():
+    #First, check for exit conditions
+    if shouldShutdown():
+        doShutdown()
         
-    if gpio.input(BTN_PHOTO):
+    #Second, check if we should begin photobooth-ing
+    if shouldStart():
 	    takePhotos()
-		
-    root.after(DELAY_MS, checkButton)
-
-def checkShutdown():
-    off = gpio.input(BTN_SHUTDOWN)
-    if off:
-        if TEST:
-            os.system("sudo shutdown -k now shutdown button pressed [testing]")
-        else:
-            os.system("sudo shutdown -h now shutdown button pressed")
-            sys.exit(0)
-        
-def doPhotoPrint(filename):
-    if TEST:
-        os.system("lp " + filename)
-        global printCount
-        printCount += 1
-
-def lightOn():
-    gpio.output(OUT_LIGHT, 1)
-
-def lightOff():
-    gpio.output(OUT_LIGHT, 0)
 	
-def takeSinglePhoto(filename):
-    lightOn()
-    camera.hflip = True
-    camera.start_preview()
-    time.sleep(5)
-    camera.hflip = False
-    camera.capture(filename)
-    camera.stop_preview()
-    lightOff()
+    #Finally, schedule ourself to run again
+    root.after(DELAY_MS, mainBody)
          
 def takePhotos():
     #Start taking Photos
     today = time.strftime("%d-%m-%Y")
     path = DIR_SAVE + today + "/"
-    print os.path.isdir(path)
     if not (os.path.isdir(path)):
         os.mkdir(path)
     
     now = time.strftime("%H%M%S")
-	
-    takeSinglePhoto(path + now + "A.jpg")
-    time.sleep(0.5)
-    takeSinglePhoto(path + now + "B.jpg")
-    time.sleep(0.5)
-    takeSinglePhoto(path + now + "C.jpg")
-    time.sleep(0.5)
-    takeSinglePhoto(path + now + "D.jpg")
+    for i in range(1, NUM_IMAGES):
+        imageName[i] = path + now + "_" + i + ".jpg"
+        takeSinglePhoto(imageName[i], 5)
+        time.sleep(0.5)
 
     try:
-        imagename1 = path + now + "A.jpg"
-        im1 = PIL.Image.open(path + now + "A.jpg")
-        im2 = PIL.Image.open(path + now + "B.jpg")
-        im3 = PIL.Image.open(path + now + "C.jpg")
-        im4 = PIL.Image.open(path + now + "D.jpg")
+        for i in range (1, NUM_IMAGES):
+            im[i] = PIL.Image.open(imageName[i])
     except:
-        print sys.exc_value
-        print "Unable to load Images"
-        print (imagename1)
+        print "Unable to load individual images"
         exit(1)
-    print "Images loaded"
-
+    
+    finalName = DIR_COMPOSITE + now + ".png"
     try:
         final = PIL.Image.open(DIR_IMAGE + "print background.png")
     except:
         print "Unable to load BG"
         exit(1)
 
-    final.paste(im1, (30,180))
-    final.paste(im2, (420,180))
-    final.paste(im3, (30,570))
-    final.paste(im4, (420,570))
+    final.paste(im[1], (30,180))
+    final.paste(im[2], (420,180))
+    final.paste(im[3], (30,570))
+    final.paste(im[4], (420,570))
+    final.save(finalName)
     
-    final.save(DIR_COMPOSITE + now + ".png")
+    doPhotoPrint(finalName)
 
-    doPhotoPrint(DIR_COMPOSITE + now + ".png")
+def doShutdown():
+    if TEST:
+        os.system("sudo shutdown -k now shutdown button pressed [testing]")
+    else:
+        os.system("sudo shutdown -h now shutdown button pressed")
+        sys.exit(0)
+    
+def doPhotoPrint(filename):
+    if not TEST:
+        os.system("lp " + filename)
+        global printCount
+        printCount += 1
+
+def takeSinglePhoto(filename, previewLength):
+    lightOn()
+    camera.hflip = True
+    camera.start_preview()
+    time.sleep(previewLength)
+    camera.hflip = False
+    camera.capture(filename)
+    camera.stop_preview()
+    lightOff()
+    
+def shouldShutdown():
+    return gpio.input(BTN_SHUTDOWN)
+    
+def shouldStart():
+    return gpio.input(BTN_PHOTO)
+        
+def lightOn():
+    gpio.output(OUT_LIGHT, 1)
+
+def lightOff():
+    gpio.output(OUT_LIGHT, 0)
