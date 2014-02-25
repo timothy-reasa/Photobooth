1f38bee87aad7803b1737168e64c74c016ade548
timothy-reasa <timothy.reasa@gmail.com>
2014-02-24 23:29:42 -0600

    rearranged file

--- photo.py
+++ photo.py
@@ -27,63 +27,56 @@ DIR_COMPOSITE = "/home/pi/Photobooth/final images/" #for final composite images 
 DIR_IMAGE = "/home/pi/Photobooth/base images/"		#for static background images
 TEST = True #True no printout and shutdown only warns
 
-#Setup gpio
-gpio.setmode(gpio.BCM)
-gpio.setup(BTN_SHUTDOWN, gpio.IN)
-gpio.setup(BTN_PHOTO, gpio.IN)
-gpio.setup(OUT_LIGHT, gpio.OUT)
-
-#Initialize GUI
-root = tk.Tk()
-root.title("Photobooth")
-#w = root.winfo_screenwidth()
-#h = root.winfo_screenheight()
-root.overrideredirect(1)
-root.geometry("660x440+0+0")
-
-pane1 = tk.Frame(root, relief=tk.RAISED, borderwidth=2)
-pane1.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)    
-
-bgImage = tk.PhotoImage(PIL.Image.open(DIR_IMAGE + "screen background.png"))
-bg = tk.Label(pane1)
-#bg.image = bgImage
-#bg.place(x=0, y=0, relwidth=1, relheight=1)
-bg.pack()
-btn1 =tk.Button(pane1, text="quit", command=btn_click)
-btn1.place(x=0, y=0)
-#btn1.pack()
-
-#Initialize PI camera
-camera=picamera.PiCamera()
-camera.preview_fullscreen = False
-camera.preview_window = (290, 100, 350, 350)
-#camera.color_effects = (128, 128)
-camera.resolution = (350, 350)
-#camera.crop = (0.5, 0.5, 1.0, 1.0)
-
-#Initialize state
-printCount = 0
-
-#Main loop
-root.after(DELAY_MS, mainBody)
-root.mainloop()
-
 def btn_click():
     gpio.cleanup()
     sys.exit(0)
 
-def mainBody():
-    #First, check for exit conditions
-    if shouldShutdown():
-        doShutdown()
+def shouldShutdown():
+    return gpio.input(BTN_SHUTDOWN)
+    
+def shouldStart():
+    return gpio.input(BTN_PHOTO)
         
-    #Second, check if we should begin photobooth-ing
-    if shouldStart():
-	    takePhotos()
-	
-    #Finally, schedule ourself to run again
-    root.after(DELAY_MS, mainBody)
-         
+def lightOn():
+    gpio.output(OUT_LIGHT, 1)
+
+def lightOff():
+    gpio.output(OUT_LIGHT, 0)
+    
+def warnOn():
+    gpio.output(OUT_WARNING, 1)
+
+def warnOff():
+    gpio.output(OUT_WARNING, 0)
+    
+def doShutdown():
+    if TEST:
+        os.system("sudo shutdown -k now shutdown button pressed [testing]")
+    else:
+        os.system("sudo shutdown -h now shutdown button pressed")
+        sys.exit(0)
+    
+def doPhotoPrint(filename):
+    global warn
+
+    if not TEST:
+        os.system("lp " + filename)
+        global printCount
+        printCount += 1
+    
+    if printCount >= MAX_PRINTS:
+        warn = True
+
+def takeSinglePhoto(filename, previewLength):
+    lightOn()
+    camera.hflip = True
+    camera.start_preview()
+    time.sleep(previewLength)
+    camera.hflip = False
+    camera.capture(filename)
+    camera.stop_preview()
+    lightOff()
+        
 def takePhotos():
     #Start taking Photos
     today = time.strftime("%d-%m-%Y")
@@ -124,48 +117,57 @@ def takePhotos():
     
     doPhotoPrint(finalName)
 
-def doShutdown():
-    if TEST:
-        os.system("sudo shutdown -k now shutdown button pressed [testing]")
-    else:
-        os.system("sudo shutdown -h now shutdown button pressed")
-        sys.exit(0)
-    
-def doPhotoPrint(filename):
-    global warn
 
-    if not TEST:
-        os.system("lp " + filename)
-        global printCount
-        printCount += 1
     
-    if printCount >= MAX_PRINTS:
-        warn = True
+#Setup gpio
+gpio.setmode(gpio.BCM)
+gpio.setup(BTN_SHUTDOWN, gpio.IN)
+gpio.setup(BTN_PHOTO, gpio.IN)
+gpio.setup(OUT_LIGHT, gpio.OUT)
 
-def takeSinglePhoto(filename, previewLength):
-    lightOn()
-    camera.hflip = True
-    camera.start_preview()
-    time.sleep(previewLength)
-    camera.hflip = False
-    camera.capture(filename)
-    camera.stop_preview()
-    lightOff()
-    
-def shouldShutdown():
-    return gpio.input(BTN_SHUTDOWN)
-    
-def shouldStart():
-    return gpio.input(BTN_PHOTO)
+#Initialize GUI
+root = tk.Tk()
+root.title("Photobooth")
+#w = root.winfo_screenwidth()
+#h = root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("660x440+0+0")
+
+pane1 = tk.Frame(root, relief=tk.RAISED, borderwidth=2)
+pane1.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)    
+
+bgImage = tk.PhotoImage(PIL.Image.open(DIR_IMAGE + "screen background.png"))
+bg = tk.Label(pane1)
+#bg.image = bgImage
+#bg.place(x=0, y=0, relwidth=1, relheight=1)
+bg.pack()
+btn1 = tk.Button(pane1, text="quit", command=btn_click)
+btn1.place(x=0, y=0)
+#btn1.pack()
+
+#Initialize PI camera
+camera=picamera.PiCamera()
+camera.preview_fullscreen = False
+camera.preview_window = (290, 100, 350, 350)
+#camera.color_effects = (128, 128)
+camera.resolution = (350, 350)
+#camera.crop = (0.5, 0.5, 1.0, 1.0)
+
+#Initialize state
+printCount = 0
+
+#Main loop
+root.after(DELAY_MS, mainBody)
+root.mainloop()
+
+def mainBody():
+    #First, check for exit conditions
+    if shouldShutdown():
+        doShutdown()
         
-def lightOn():
-    gpio.output(OUT_LIGHT, 1)
-
-def lightOff():
-    gpio.output(OUT_LIGHT, 0)
-    
-def warnOn():
-    gpio.output(OUT_WARNING, 1)
-
-def warnOff():
-    gpio.output(OUT_WARNING, 0)
+    #Second, check if we should begin photobooth-ing
+    if shouldStart():
+	    takePhotos()
+	
+    #Finally, schedule ourself to run again
+    root.after(DELAY_MS, mainBody)
