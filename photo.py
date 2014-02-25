f8453dffcfad7045fc9a5e20c6501538d0a25368
timothy-reasa <timothy.reasa@gmail.com>
2014-02-24 23:24:58 -0600

    test

--- photo.py
+++ photo.py
@@ -45,7 +45,7 @@ pane1 = tk.Frame(root, relief=tk.RAISED, borderwidth=2)
 pane1.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)    
 
 bgImage = tk.PhotoImage(PIL.Image.open(DIR_IMAGE + "screen background.png"))
-bg = tk.Label(pane1, image=bgImage)
+bg = tk.Label(pane1)
 #bg.image = bgImage
 #bg.place(x=0, y=0, relwidth=1, relheight=1)
 bg.pack()
